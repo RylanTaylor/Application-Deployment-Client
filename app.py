d038e4e3da59d97fb16340f6418138f84a8a1e98
RylanTaylor <rylan@rylantaylor.com>
2021-01-08 09:27:22 -0700

    Added compatibility with Application Deployment Server 2.1.0's status page along with other minor enhancments

--- app.py
+++ app.py
@@ -3,149 +3,162 @@ import eel
 import os
 import sys
 import json
-import sys
 import subprocess
 import ctypes
 import time
-from pathlib import Path
+import sys
+import math
 
-default_server = "INSERT_DEFAULT_SERVER_HERE"
-default_port = "177055"
+server = "INSERT_DEFAULT_SERVER_HERE"
+port = "8080"
+
+app_port = 17755
+minToTimeout = 10 #Number of minutes to check status before timing out
 
 run_program = True
 
-try:
-	
-	#Suite variables
+def errorNotice(message): #Logs error then shows message with error before exiting program.
+	log("ERROR: " + message)
+	ctypes.windll.user32.MessageBoxExW(0, message, "Error", 0x0 | 0x10)
+	sys.exit(42)
 
-	suite_files = os.environ.get("localappdata") + "\\ads-client\\"
-	suite_programs = suite_files + "programs\\" 
-	suite_settings = suite_files + "settings\\"
-	suite_logs = suite_files + "logs\\"
-
-	suite_general_settings = suite_settings + "settings.json"
-
-	program_location = suite_programs + "application-deployment-client\\"
-	program_settings = suite_settings + "application-deployment-client.json"
-	program_logs = suite_logs + "application-deployment-client.log"
-
-	Path(suite_programs).mkdir(parents=True, exist_ok=True)
-	Path(suite_settings).mkdir(parents=True, exist_ok=True)
-	Path(suite_logs).mkdir(parents=True, exist_ok=True)
-	Path(program_location).mkdir(parents=True, exist_ok=True)
-
-	#Functions
-
-	def log(message): # Logging function
+def log(message): # Logging function
+	log_folder = "./logs"
+	if not os.path.exists(log_folder):
+		os.mkdir(log_folder)
+	program_logs = log_folder + "/new-computer-setup.log"
+	try:
 		if not os.path.exists(program_logs):
 			open(program_logs, "w+").write("")
 		with open(program_logs, "a+") as f:
 			f.write("(" + str(round(time.time() * 1000)) + ") " + message + "\n")
+			print(message)
+	except Exception as e: #Uses %temp%\adsclient.log if the log file cannot be created or used
+		open(os.environ.get("temp") + "\\adsclient.log", "a+").write("(" + str(round(time.time() * 1000)) + ") " + "ADS Client: Unable to save to log file \"" + program_logs + "\" due to the following error: " + str(e) + "\n")
+		ctypes.windll.user32.MessageBoxExW(0, "Unable to save logs, please check log path. Please check %temp%\\adsclient.log for additional information.", "Error", 0x0 | 0x10)
+		sys.exit(42)
 
-	def get_user(): # Get user, check default suite credentials but default to computer credentials if all else fails
-		if os.path.exists(suite_general_settings):
-			with open(suite_general_settings) as f:
-				return json.loads(f.read())["Credentials"]["Username"]
-		else:
-			return os.environ.get("username")
+try: #Check if ADS client is already running by pinging the application port on localhost
+	r = requests.get("http://localhost:" + str(app_port) + "/")
+	if r.text:
+		run_program = False
+		message = "New Computer Setup is already running, please close open instance before starting new one."
+		log(message)
+		errorNotice(message)
+except:
+	pass
 
+def indexSetup(): #Setup default index file by reading the template file and making appropriate changes before saving
+	err = None
 
-	if not os.path.exists(program_settings): #Check if server settings are present, save default settings if not
-		with open(program_settings, "w+", encoding="utf-8") as f:
-			server_info = {"Server" : default_server, "Port" : default_port}
+	with open("./web/template/template_index.html", "r") as f:
+		index = f.read()
 
-			f.write(json.dumps(server_info, indent=4))
+	#Replaces text in index with the current client version and max install time
+	clientver = open("ver.txt", "r", encoding="utf-8").read()
+	index = index.replace("REPLACE_WITH_VERSION_NUM", "Version " + clientver)
+	index = index.replace("MAX_TIME_TO_INSTALL", str(math.ceil(minToTimeout)))
 
-
-	server = None
-	port = None
-
-	with open(program_settings, "r") as f: #Get server information
-		settings = json.loads(f.read())
-		
-		server = settings["Server"]
-		port = settings["Port"]
-
-	def indexSetup(): #Setup default index file
-		cont = False
-		err = None
-
-		with open("./web/template/template_index.html", "r") as f:
-			index = f.read()
-
-		try:
-			set_ver = "Version " + open("./ver.txt", "r", encoding="utf-8").read()
-		except:
-			set_ver = ""
-
-		index = index.replace("REPLACE_WITH_VERSION_NUM", set_ver)
-
-		try:
-			r = requests.get("http://" + server + ":" + port, params={"get":"softwareInfo", "user":get_user()})
-			if r.status_code == 200:
-				log("Recieved software information from server.")
-				cont = True
-			else:
-				output = "Server status code returned \"" + r.status_code + "\"."
-				log(output)
-				err = output
-		except Exception as e:
-			log(err)
-			err = str(e)
-		
-		if cont:
-			softwareStacks = r.json()
-
-			dataString = ""
-			for item in softwareStacks["Software Stacks"]:
-				if item != "Defaults":
-					dataString += "<option value=\"" + item + "\" "
-					if item == "Standard":
-						dataString += "selected"
-					dataString += ">" + item + "</option>\n"
-			
-			index = index.replace("REPLACE_WITH_STACK_INFO", dataString)
-		else:
-			index = "<center><h1>Unable to connect to Application Deployment Server, please contact your administrator.</h1><br />Error information:<br /><textarea style=\"width:40%;height:5%\">server=" + server + ";port=" + port +";error=" + err + "</textarea></center>"
-		with open("./web/index.html", "w+") as f:
-				f.write(index)
-
-	if run_program: #Start program
-		indexSetup()
-		eel.init("./web")
-
-	@eel.expose
-	def runPing(asset):
-		return 0
-
-	@eel.expose
-	def runInstall(asset, stack, user):
-
-		log("Running install: asset=" + asset + "; user=" + user + "; stack=" + stack)
-
-		r = requests.post(
-			"http://" + server + ":" + port,
-			data={"asset":asset, "stack":stack, "user":user, "requser":get_user()},
-			timeout=None #Waits forever to timeout
-			)
-
+	try:
+		r = requests.get("http://" + server + ":" + port, params={"get":"softwareInfo", "user":os.environ.get("username"), "client":clientver}) #Retrieve stack information from ADS server
 		if r.status_code == 200:
-			output = "All programs successfully installed on " + asset + "!"
-			log(output)
-			return output
+			log("Recieved the following software information from server: " + r.text.strip().replace("\n","").replace("    ","").replace("\t",""))
 		else:
-			output = "ERROR: " + r.text
+			if r.text != "":
+				output = "Failed connecting to server with message: \"" + r.text + "\"."
+			else:
+				output = "Failed connecting to server with status code \"" + str(r.status_code) + "\"."
 			log(output)
-			return output
+			err = output
+	except Exception as e:
+		err = str(e)
+	
+	if not err: #Adds software stacks to index
+		softwareStacks = r.json()
 
-	def close_callback_func(one,two):
-		sys.exit(0)
+		dataString = ""
+		for item in softwareStacks["Software Stacks"]:
+			if item != "Defaults":
+				dataString += "<option value=\"" + item + "\" "
+				if item == "Standard":
+					dataString += "selected"
+				dataString += ">" + item + "</option>\n"
+		
+		index = index.replace("REPLACE_WITH_STACK_INFO", dataString)
+	else:
+		if "does not meet the server's minimum required version" in err: #Checks if error is a client version issue
+			message = "Your client is out of date! An update is required before continuing."
+		else:
+			message = "Unable to connect to the application deployment server, check log file for additional information."
+		run_program = False
+		log("ERROR: " + message + " Error information: server=" + server + ";port=" + port +";error=" + err)
+		errorNotice(message)
 
-	if run_program:
-		try:
-			eel.start("index.html",size=(800,800),close_callback=close_callback_func,port=17755)
-		except (SystemExit, MemoryError, KeyboardInterrupt):
-			pass
+	with open("./web/index.html", "w+") as f:
+		f.write(index)
+
+def checkStatus(asset, user): #Checks status page of ADS server to see if it has completed running
+	try:
+		r = requests.get("http://" + server + ":" + port + "/status", params={"get":asset, "user":os.environ.get("username")})
+		log("Server returned the following status code for \"" + asset + "\": " + str(r.status_code))
+		if not r.status_code == 102: #A return code of 102 means it's still running
+			return r.text
+		else:
+			return None
+	except:
+		return "Unable to connect to status page, check log file for additional information."
+	
+
+#Start program
+indexSetup()
+try:
+	eel.init("web")
 except Exception as e:
-	ctypes.windll.user32.MessageBoxW(0, "Error: " + str(e), "Error", 1)
-	log("Error: " + str(e))
+	message = "Error: " + str(e)
+	log(message)
+	run_program = False
+	errorNotice(message)
+
+@eel.expose
+def runInstall(asset, stack, user):
+
+	log("Running install: asset=" + asset + "; user=" + user + "; stack=" + stack)
+
+	r = requests.post(
+		"http://" + server + ":" + port,
+		data={"asset":asset, "stack":stack, "user":user, "requser":os.environ.get("username")},
+		timeout=60.0 #Waits a minute to timeout
+		)
+
+	if r.status_code == 202: #Status code 202 means the server has accepted and begun the installation
+		res = None
+
+		checkInterval = 3 #Number of seconds to wait between status checks
+		totalChecks = int(60 / checkInterval * minToTimeout)
+
+		for i in range(totalChecks): #Makes requests to the status server the number of times requested in the global variable "minToTimeout" with the interval set above in "checkInterval"
+			x = i + 1
+			eel.sleep(checkInterval)
+			log("Checking status of \"" + asset + "\" (Check " + str(x) + "/" + str(totalChecks) + ")")
+			res = checkStatus(asset, user)
+			if res:
+				break
+
+		if res:
+			output = res
+		else:
+			output = "ERROR: No response from server after " + str(minToTimeout) + " minutes, check ADS server logs for more information."
+		
+		log("Status of \"" + asset + " returned the following: " + output)
+
+		return output
+	else:
+		output = "ERROR: " + r.text
+		log(output)
+		return output
+
+if run_program: #run_program is set to True if there are no errors in the index and program setup, if this is True then start the eel app
+	try:
+		eel.start("index.html",size=(800,800),port=app_port)
+	except Exception as e:
+		errorNotice("Unable to start program due to the following error: " + str(e))
--- readme.md
+++ readme.md
@@ -1,28 +1,24 @@
-# Rylan's Application Deployment Client
+# Application Deployment Client
 
-This program is meant to work in conjunction with Rylan's Application Deployment Server. This program will work with the server to retrieve all software stacks and install the requested stack on the machine requested.
+This program is meant to work in conjunction with the Application Deployment Server found in the requirements section. This program will work with the server to retrieve all software stacks and install the requested stack on any supported machine.
 
 ## Requirements
-This software was created with and requires Python 3.6+ as well as a Chromium browser as the frontend is supported by Eel which uses the Chromium browser for the front end. This client also requires Rylan's Application Deployment server for use which can found found on the following page:
+This software was created with and requires Python 3.6+ as well as a Chromium browser as the frontend is supported by Eel which uses the Chromium browser. This client also requires the Application Deployment Server for use which can found found on the following page:
 
 >[https://github.com/RylanTaylor/Application-Deployment-Server](https://github.com/RylanTaylor/Application-Deployment-Server)
 
 ## Installation
 
-Download the latest release on GitHub and after doing so edit "app.py" changing the two variable "default_server" and "default_port" to point toward the hosted Rylan's Application Deployment Server server. After doing so, run the following commands in the downloaded folder:
+Download the latest release on GitHub and after doing so edit "app.py" changing the two variable "server" and "port" to point toward the hosted Application Deployment Server api. After doing so, run the following commands in the downloaded folder:
 
 ```bash
 pip install -r requirements.txt
 python3 app.py
 ```
-By changing the variables in "Suite variables" and adding "ver.txt" this can also be used in conjunction with Rylan Software Suite:
-
->[https://github.com/RylanTaylor/Software-Suite-Updater](https://github.com/RylanTaylor/Software-Suite-Updater)
 
 ## Usage
-Upon running the application, put the computer name in the "Computer Host Name" field to point the installation server to that machine. The "Owner's Username" field is included in the client but is not used by default in the Application Deployment server. Set this field to Null by default if this is not necessary and remove the entry from "./web/template/template_index.html".
+Upon running the application, put the computer name in the "Computer Host Name" field to point the installation server to that machine. The "Owner's Username" field is used for logging purposes only and is not a required field, leave blank if needed. Select a software stack to install on the machine then click "Submit" to begin the installation. After the installation has completed an alert will be shown with the status.
 
- 
 ## Contributing
 Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
 
